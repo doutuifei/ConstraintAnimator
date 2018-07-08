2ba98bf735843fcd2967971c90ca5a59c869b836
muzi <727784430@qq.com>
2018-07-08 21:36:13 +0800

    layout5

--- app/src/main/java/com/muzi/constraintanimator/MainActivity.java
+++ app/src/main/java/com/muzi/constraintanimator/MainActivity.java
@@ -3,61 +3,27 @@ package com.muzi.constraintanimator;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
-import android.support.constraint.ConstraintLayout;
-import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
-import android.transition.TransitionManager;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ConstraintSet first, second, third, fourth;
-    private ConstraintLayout firstLayout;
     private Button start;
-    int index = 1;
+    private ResponderView responderView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout1);
-        first = new ConstraintSet();
-        second = new ConstraintSet();
-        third = new ConstraintSet();
-        fourth = new ConstraintSet();
-        firstLayout = findViewById(R.id.contentPanel);
+        setContentView(R.layout.activity_main);
+        responderView = findViewById(R.id.responderView);
         start = findViewById(R.id.start);
 
-        first.clone(firstLayout);
-        second.clone(this, R.layout.layout2);
-        third.clone(this, R.layout.layout3);
-        fourth.clone(this, R.layout.layout4);
         start.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View view) {
-                switch (index) {
-                    case 1:
-                        index = 2;
-                        TransitionManager.beginDelayedTransition(firstLayout);// 动画效果
-                        second.applyTo(firstLayout);
-                        break;
-                    case 2:
-                        index = 3;
-                        TransitionManager.beginDelayedTransition(firstLayout);// 动画效果
-                        third.applyTo(firstLayout);
-                        break;
-                    case 3:
-                        index = 4;
-                        TransitionManager.beginDelayedTransition(firstLayout);
-                        fourth.applyTo(firstLayout);
-                        break;
-                    case 4:
-                        index = 1;
-                        TransitionManager.beginDelayedTransition(firstLayout);
-                        first.applyTo(firstLayout);
-                        break;
-                }
+                responderView.addView();
             }
         });
     }
---
+++ app/src/main/java/com/muzi/constraintanimator/ResponderView.java
@@ -0,0 +1,89 @@
+package com.muzi.constraintanimator;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.transition.TransitionManager;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+
+/**
+ * 作者: lipeng
+ * 时间: 2018/7/8
+ * 邮箱: lipeng@moyi365.com
+ * 功能:
+ */
+public class ResponderView extends ConstraintLayout {
+
+    private ConstraintSet first, second, third, fourth, fifth;
+    private ConstraintLayout firstLayout;
+    private int index = 1;
+
+    public ResponderView(Context context) {
+        super(context);
+        init();
+    }
+
+    public ResponderView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        LayoutInflater.from(getContext()).inflate(R.layout.layout1, this, true);
+        firstLayout = findViewById(R.id.contentPanel);
+
+        first = new ConstraintSet();
+        second = new ConstraintSet();
+        third = new ConstraintSet();
+        fourth = new ConstraintSet();
+        fifth = new ConstraintSet();
+
+        first.clone(firstLayout);
+        second.clone(getContext(), R.layout.layout2);
+        third.clone(getContext(), R.layout.layout3);
+        fourth.clone(getContext(), R.layout.layout4);
+        fifth.clone(getContext(), R.layout.layout5);
+    }
+
+
+    public void addView() {
+        changeView();
+    }
+
+    public void stubView() {
+
+    }
+
+    private void changeView() {
+        switch (index) {
+            case 1:
+                index = 2;
+                TransitionManager.beginDelayedTransition(firstLayout);// 动画效果
+                second.applyTo(firstLayout);
+                break;
+            case 2:
+                index = 3;
+                TransitionManager.beginDelayedTransition(firstLayout);// 动画效果
+                third.applyTo(firstLayout);
+                break;
+            case 3:
+                index = 4;
+                TransitionManager.beginDelayedTransition(firstLayout);
+                fourth.applyTo(firstLayout);
+                break;
+            case 4:
+                index = 5;
+                TransitionManager.beginDelayedTransition(firstLayout);
+                fifth.applyTo(firstLayout);
+                break;
+            default:
+                index = 1;
+                TransitionManager.beginDelayedTransition(firstLayout);
+                first.applyTo(firstLayout);
+                break;
+        }
+    }
+
+
+}
